d4d87741b67ab39785f3233dedc27c83de240f73
Fredy Ismael López Damián <lopz_damian@hotmail.com>
2018-01-09 11:01:59 -0600

    Modelo del cliente
    Información del cliente

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -21,4 +21,20 @@ class Login_model extends CI_Model {
 			redirect(base_url().'login','refresh');
 		}
 	}
+
+	public function login_user2($usuario,$contraseña)
+	{
+		$this->db->where('usuario',$usuario);
+		$this->db->where('contraseña',$contraseña);
+		$this->db->select('*');
+		$this->db->from ('eg_users');
+		$this->db->join('eg_membresia', 'eg_membresia.nu_user=eg_users.id');
+		$query = $this->db->get();
+
+		if($query->num_rows() == 1)
+		{
+			return $query->row();
+		}
+	}
+
 }
