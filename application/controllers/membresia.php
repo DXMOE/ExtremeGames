10d058e9f144e4217150adad2598a937d365f1d6
Jesus Alberto Perez Perez <chema1996_@hotmail.com>
2018-01-09 10:48:39 -0600

    controlador mebresia

--- application/controllers/membresia.php
+++ application/controllers/membresia.php
@@ -6,19 +6,23 @@ class Membresia extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model('membresia_model');
+        $this->load->database();
+        $this->load->helper('url');
+         $this->load->library('form_validation');
     }
 
     public function index()
-	{
-            $data = array(
-                
-                  'eg_saldo' => $this->membresia_model->get_eg_saldo()
-                    
-            );
-            
-            
-        $this->load->view('membresia',$data);
+{
+        
        
+        $data['eg_users'] = $this->membresia_model->get_eg_users();
+$data['eg_membresia'] = $this->membresia_model->get_eg_membresia(); 
+
+$this->load->view('membresia', $data);
+        
+        
+        
+           
         
         }
         
@@ -26,10 +30,43 @@ class Membresia extends CI_Controller {
         
         public function add_eg_membresia()
 {
+            
     if($this->input->post('submit'))
     {
+        $this->load->view('membresia',$data);
         $this->membresia_model->add_eg_membresia();
-        redirect(base_url().'membresia');
+        redirect(base_url().'membresia',$data);
     }
 }
+
+
+public function edit_eg_membresia($id)
+{
+   
+   $verificar = $this->membresia_model->get_eg_membresia_id($id);
+   if($this->input->post('submit'))
+   {
+       $this->membresia_model->update();
+       redirect(base_url().'membresia');
+   }
+    if( $verificar == false)        
+    {
+        $data = array(
+            'titulo' => 'Editar eg_membresia',
+            'error'=> 'esta membresia no esxiste'
+        );
+        
+    }
+      else {
+          $data = array(
+            'titulo' => 'Editar eg_membresia',
+              
+            'nu_extra'=> $verificar['nu_extra'],
+              'nu_saldo'=> $verificar['nu_saldo'],
+              'nu_puntos'=> $verificar['nu_puntos'],
+              
+              'nu_user'=> $verificar['nu_user']
+        );
+    $this->load->view('edit_membresia', $data);
+      }  }
 }
